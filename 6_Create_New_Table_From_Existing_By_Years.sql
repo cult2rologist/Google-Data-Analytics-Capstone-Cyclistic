07dd930cf06bd6a7d3e280b8c3bea5150359525d
Ilya Mckay <104564914+cult2rologist@users.noreply.github.com>
2023-10-31 23:20:32 -0300

    Update 6_Create_New_Table_From_Existing_By_Years.sql

--- 6_Create_New_Table_From_Existing_By_Years.sql
+++ 6_Create_New_Table_From_Existing_By_Years.sql
@@ -1,11 +1,39 @@
 CREATE TABLE cyclistic_2022 AS
-SELECT *
+SELECT ride_id,
+	   rideable_type,
+	   started_at::date AS s_ride_date,
+	   started_at::time AS s_ride_time,
+	   ended_at::date AS e_ride_date,
+	   ended_at::time AS e_ride_time,
+	   start_station_name,
+	   start_station_id,
+	   end_station_name,
+	   end_station_id,
+	   start_lat,
+	   start_lng,
+	   end_lat,
+	   end_lng,
+	   member_casual
 FROM cyclist_clean
 WHERE started_at::date > date '2022-01-01 00:00:00'
 ;
 
 CREATE TABLE cyclistic_2021 AS
-SELECT *
+SELECT ride_id,
+	   rideable_type,
+	   started_at::date AS s_ride_date,
+	   started_at::time AS s_ride_time,
+	   ended_at::date AS e_ride_date,
+	   ended_at::time AS e_ride_time,
+	   start_station_name,
+	   start_station_id,
+	   end_station_name,
+	   end_station_id,
+	   start_lat,
+	   start_lng,
+	   end_lat,
+	   end_lng,
+	   member_casual
 FROM cyclist_clean
 WHERE started_at::date < date '2022-01-01 00:00:00'
 ;
